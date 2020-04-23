37aefb928c4c61f59d94d1a9c2d0fb8a60d45ac6
Saurabh <saurabhbharti9@gmail.com>
2020-04-23 12:45:29 +0530

    added webpack config to express app

--- server/index.jsx
+++ server/index.jsx
@@ -1,9 +1,26 @@
 import express from "express";
 import yields from "express-yields";
 import fs from "fs-extra";
+import webpack from "webpack";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+//start webpack
+if (process.env.NODE_ENV === "development") {
+  const config = require("../webpack.config.dev.babel").default;
+  //compile config
+  const compiler = webpack(config);
+
+  //add dev-middleware
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      noInfo: true,
+    })
+  );
+  //add hot-reload middleware
+  app.use(require("webpack-hot-middleware")(compiler));
+}
 
 app.get(["/"], function* (req, res) {
   let index = yield fs.readFile("./public/index.html", "utf-8");
