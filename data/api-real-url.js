51063f445d51295b57f42f6164f0b18fa42cee0f
Saurabh <saurabhbharti9@gmail.com>
2020-04-28 10:07:02 +0530

    build failed

--- data/api-real-url.js
+++ data/api-real-url.js
@@ -1,6 +1,5 @@
 export const questions = `https://api.stackexchange.com/2.0/questions?site=stackoverflow`;
 export const question = (id) =>
   `https://api.stackexchange.com/2.0/questions/${id}?site=stackoverflow&filter=withbody`;
-//https://api.stackexchange.com/2.0/tags/data-structure/faq?site=stackoverflow
 export const tags = (tag) =>
   `https://api.stackexchange.com/2.0/tags/${tag}/faq?site=stackoverflow`;
