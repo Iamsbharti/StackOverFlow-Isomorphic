2380c0edc8cce461b011affbacfa1ffb54f2ca4c
Saurabh <saurabhbharti9@gmail.com>
2020-05-04 21:34:00 +0530

    added snapshots tests for Tags

---
+++ src/components/__tests__/QuestionsTag.js
@@ -0,0 +1,13 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import QuestionsTag from "../QuestionsTag";
+import { Link } from "react-router-dom";
+
+/*describe("Test QuestionTag component using snapshot-test", () => {
+  const questionTagTree = renderer.create(
+    <QuestionsTag tags={["react", "html"]} />
+  );
+  console.log(questionTagTree);*/
+it("should test a snapshot test", () => {
+  console.log("snapshot test");
+});
