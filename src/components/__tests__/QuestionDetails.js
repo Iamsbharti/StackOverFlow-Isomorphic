c56ddcf5e80a3fe75449d4328586f7a786eb5a1a
Saurabh <saurabhbharti9@gmail.com>
2020-05-04 09:57:56 +0530

    added eg on jest globals

--- src/components/__tests__/QuestionDetails.js
+++ src/components/__tests__/QuestionDetails.js
@@ -1,3 +1,20 @@
 describe("The QuestionList Component", () => {
-  it("should render list of questions", () => {});
+  beforeEach(() => {
+    console.log("Runs before each test");
+  });
+  beforeAll(() => {
+    console.log("runs before all tests");
+  });
+  afterAll(() => {
+    console.log("after all tests");
+  });
+  afterEach(() => {
+    console.log("after each test");
+  });
+  it("should render list of questions", () => {
+    expect(40 + 2).toBe(42);
+  });
+  it("should be a meaning of life", () => {
+    expect(40 + 2).toEqual(42);
+  });
 });
